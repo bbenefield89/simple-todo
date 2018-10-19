d1444876a2bf935495b53899308f5935e9a30421
bbenefield89 <bsquared18@gmail.com>
2018-10-19 19:51:32 -0400

    REUSABLE COMP '<Button />'
    - frontend/components/misc/Button.js

---
+++ frontend/components/misc/Button.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Button = ({ onClick, buttonText }) => {
+  return (
+    <button
+      onClick={ onClick }>
+        { buttonText }
+    </button>
+  )
+}
+ 
+export default Button
