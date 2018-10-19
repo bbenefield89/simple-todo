c06304c22c568c5a7b61c40c81dc06112fdee772
bbenefield89 <bsquared18@gmail.com>
2018-10-19 19:53:56 -0400

    NEW METHOD 'update' RETURNS AN UPDATED TASK
    - server/todo-data.js
      - new flag on 'todos', 'completed', handles whether or not a todo has been completed

--- server/todo-data.js
+++ server/todo-data.js
@@ -1,7 +1,8 @@
 let todosCreated = 1;
 let todos = [{
 	id: 1,
-	text: 'Finish coding exercise',
+  text: 'Finish coding exercise',
+  completed: false
 }];
 
 export default class TodoData {
@@ -19,17 +20,20 @@ export default class TodoData {
 
 	static delete(id) {
 		return new Promise((resolve, reject) => {
-			const todoIndex = todos.findIndex(todo => todo.id.toString() === id.toString());
+      const todoIndex = todos.findIndex(todo => todo.id.toString() === id.toString());
 			if (todoIndex < 0 || todoIndex >= todos.length) return reject();
 			todos.splice(todoIndex, 1);
 			resolve();
 		})
 	}
 
-	static update(id, todo) {
+	static update(id) {
 		return new Promise((resolve, reject) => {
-			// TODO: Implement
-			reject('Not Implemented');
+      const todo = todos.find(todo => String(todo.id) === String(id))
+
+      todo.completed = !todo.completed
+      
+      resolve({ todo })
 		})
 	}
 }
